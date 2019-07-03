d0e570557fa9d1f2fa89175b7dbfcf929207d356
Владислав Горохов <gbg.0101@mail.ru>
2019-07-03 17:07:01 +0600

    AddAdapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,53 @@
+﻿// TODO: восстановить работоспособность кода, чтобы он выводил следующий результат в консоли
+/*
+Client: I get a simple component:
+RESULT: ConcreteComponent
+
+Client: Now I've got a decorated component:
+RESULT: ConcreteDecoratorB(ConcreteDecoratorA(ConcreteComponent))
+Press any key to continue . . .
+*/
+
+using System;
+
+namespace TODO.DesignPatterns.Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Traveler traveler = new Camel(car);
+
+            Console.WriteLine(traveler.Travel());
+        }
+    }
+
+    public class Camel : Traveler
+    {
+        private readonly Car _car;
+
+        public Camel(Car car)
+        {
+            _car = car;
+        }
+
+        public string Travel()
+        {
+            return $"Camel is {_car.Move()}";
+        }
+    }
+
+    public interface Traveler
+    {
+        string Travel();
+    }
+
+    public class Car
+    {
+        public string Move()
+        {
+            return "moving";
+        }
+    }
+}
---
+++ AdapterTemplate/AdapterTemp.cs
@@ -0,0 +1,55 @@
+﻿// Паттерн Адаптер
+//
+// Назначение: Позволяет объектам с несовместимыми интерфейсами работать вместе.
+
+using System;
+
+namespace DesignPatterns.Adapter.Conceptual
+{
+    // Целевой класс объявляет интерфейс, с которым может работать
+    // клиентский код.
+    public interface ITarget
+    {
+        string GetRequest();
+    }
+
+    // Адаптируемый класс содержит некоторое полезное поведение, но его
+    // интерфейс несовместим  с существующим клиентским кодом. Адаптируемый
+    // класс нуждается в некоторой доработке,  прежде чем клиентский код сможет
+    // его использовать.
+    class Adaptee
+    {
+        public string GetSpecificRequest()
+        {
+            return "Specific request.";
+        }
+    }
+
+    // Адаптер делает интерфейс Адаптируемого класса совместимым с целевым
+    // интерфейсом.
+    class Adapter : ITarget
+    {
+        private readonly Adaptee _adaptee;
+
+        public Adapter(Adaptee adaptee)
+        {
+            this._adaptee = adaptee;
+        }
+
+        public string GetRequest()
+        {
+            return $"This is '{this._adaptee.GetSpecificRequest()}'";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adaptee adaptee = new Adaptee();
+            ITarget target = new Adapter(adaptee);
+
+            Console.WriteLine(target.GetRequest());
+        }
+    }
+}
